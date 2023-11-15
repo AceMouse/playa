48e07ef28792447319d7779fa1a975d35bf0ba64
AceMouse <humle000@gmail.com>
2023-11-15 01:05:48 +0100

    add pause and skip 15 sec back and forth

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import subprocess
 import time 
 import os
 import sys 
+from pytimedinput import timedKey
 
 def _print(x):
     for _ in range(3):
@@ -13,6 +14,10 @@ def _print(x):
         break
 
 debug = False
+
+def popen(cmd):
+    d = {'args': cmd} if debug else {'args': cmd,'stdout':subprocess.DEVNULL, 'stderr':subprocess.DEVNULL}
+    return subprocess.Popen(**d)
 
 def run(cmd):
     d = {'args': cmd,'capture_output':True} if debug else {'args': cmd,'capture_output':True}
@@ -27,6 +32,10 @@ def signal_handler(sig, frame):
         frame.f_locals['p'].terminate()
     elif 'p' in frame.f_globals:
         frame.f_globals['p'].terminate()
+    if 'player_p' in frame.f_locals:
+        frame.f_locals['player_p'].kill()
+    elif 'player_p' in frame.f_globals:
+        frame.f_globals['player_p'].kill()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -37,8 +46,6 @@ def format_time(sec):
     sec = sec%60
     return f"{hr:02}:{min:02}:{sec:02}"
 
-LINE_UP = '\033[1A'
-LINE_CLEAR = '\x1b[2K'
 def update_t(folder,speed,dur):
     t = 0
     len = 20
@@ -72,9 +79,61 @@ def play_ch(folder,speed,book):
         _print(f"{mp3_fp} not found, retrying in 10s")
         time.sleep(10)
     dur = int(run(["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", mp3_fp]).stdout.decode().split('.')[0])+1
+
     p = Process(target=update_t, args=(working,speed,dur))
     p.start()
-    run(["ffplay","-af",f"atempo={speed}", "-nodisp", "-autoexit", "-stats", "-ss", f"{t}s", mp3_fp])
+    player_p = popen(["ffplay","-af",f"atempo={speed}", "-nodisp", "-autoexit", "-stats", "-ss", f"{t}s", mp3_fp])
+    paused = False 
+    while True:
+        while True:
+            try:
+                with open(f"{working}/t.txt","r") as tf:
+                    z = tf.read()
+                    #print(f"\n\nt: '{z}'\n\n")
+                    t = int(z)
+                    break
+            except Exception as e:
+                print(e)
+                continue
+
+        if dur-t <= 0:
+            break
+
+        x, timedOut = timedKey(timeout=-1 if paused else int((dur-t)/speed), allowCharacters=f" pt{KEY_LEFT}{KEY_RIGHT}")
+        if timedOut:
+            break
+
+#        print("key:", x)
+        if x in ' p':
+            if paused:
+                os.kill(player_p.pid, signal.SIGCONT)
+                os.kill(p.pid, signal.SIGCONT)
+            else:
+                os.kill(player_p.pid, signal.SIGSTOP)
+                os.kill(p.pid, signal.SIGSTOP)
+            paused = not paused
+        if x == 't':
+            player_p.terminate()
+            p.terminate()
+            quit(0)
+        if x == KEY_LEFT:
+            with open(f"{working}/t.txt","r") as tf:
+                t = int(tf.read())
+            with open(f"{working}/t.txt","w") as tf:
+                t = max(t-15,0)
+                tf.write(str(t))
+            player_p.terminate()
+            player_p = popen(["ffplay","-af",f"atempo={speed}", "-nodisp", "-autoexit", "-stats", "-ss", f"{t}s", mp3_fp])
+        if x == KEY_RIGHT:
+            with open(f"{working}/t.txt","r") as tf:
+                t = int(tf.read())
+            with open(f"{working}/t.txt","w") as tf:
+                t = min(t+15,dur)
+                tf.write(str(t))
+            player_p.terminate()
+            player_p = popen(["ffplay","-af",f"atempo={speed}", "-nodisp", "-autoexit", "-stats", "-ss", f"{t}s", mp3_fp])
+
+    player_p.wait()
     p.terminate()
     with open(f"{working}/t.txt","w") as tf:
         tf.write(str(0))
@@ -113,4 +172,12 @@ def play():
     while True:
         play_ch(folder, speed, book)
 
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
+ARROW_PRECURSOR_1 = '\033'
+ARROW_PRECURSOR_2 = '['
+KEY_UP    = 'A'
+KEY_LEFT  = 'D'
+KEY_DOWN  = 'B'
+KEY_RIGHT = 'C'
 play() 
